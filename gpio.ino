491e42c951e549682c2c8faa744db9d40500316a
invictus1123 <vnguyen5@stanford.edu>
2016-10-04 21:34:01 -0700

    Create gpio.ino

---
+++ gpio.ino
@@ -0,0 +1,29 @@
+/*  File: gpio.ino
+ *  Written by Vinh Nguyen & LC Tao
+ *  EE107, Sachin Katti, Fall 2016
+ *  This file implements a basic GPIO driver.
+ *  Functionality: 
+ *  -Toggle pin mode.
+ *  -Digital write.
+ */
+void gpio_set_mode(uint8_t pin, uint8_t out_or_in);
+void gpio_set_out(uint8_t pin, uint8_t on_or_off);
+void gpio_toggle_out(uint8_t pin); 
+
+void setup() {
+  //gpio_set_mode(pin,val)
+}
+
+void loop() {
+  //gpio_toggle_out(pin)
+
+}
+void gpio_set_mode(uint8_t pin, uint8_t out_or_in) {
+  
+}
+void gpio_set_out(uint8_t pin, uint8_t on_or_off) {
+  
+}
+void gpio_toggle_out(uint8_t pin) {
+  
+}
