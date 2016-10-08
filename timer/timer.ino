c1bb9bcf136ab50e6448730c3b5675315d229396
invictus1123 <vnguyen5@stanford.edu>
2016-10-07 17:22:28 -0700

    Update timer.ino

--- timer/timer.ino
+++ timer/timer.ino
@@ -10,16 +10,15 @@
  */
 void timer_init();
 void timer_reset();
-void timer_set(uint8_t period);
+void timer_set(uint16_t period);
 
 int counter_max;
 volatile uint8_t counter;
+volatile uint8_t cycle_count;
+
 ISR(TIMER0_COMPA_vect){
-    counter += 1;
-    if (counter == counter_max){
-        Serial.println("Counter max reached");
-        counter = 0;
-    }
+//    counter += 1;
+      cycle_count++;
 }
 
 void setup() {
@@ -29,15 +28,16 @@ void setup() {
 }
 
 void loop() {
-    //Serial.println(counter);
-    Serial.println(TCNT0);
+    Serial.println(cycle_count);
+//    Serial.println(TCNT0);
 }
 
 void timer_init(){
+    TIMSK0 = B00000010;
     // Toggle OC0A on match, CTC mode
     TCCR0A = B01000010;
-    // Select clock without prescaling
-    TCCR0B = B00000001;
+    // Select clock with 1024 prescaler
+    TCCR0B = B00000101;
     // Reset timer counter
     TCNT0 = B00000000;
 }
@@ -47,8 +47,7 @@ void timer_reset(){
     TCNT0 = B00000000;
 }
 
-void timer_set(uint8_t period){
-    OCR0A = (1 << 7);
-    counter_max = period;
+void timer_set(uint16_t period){
+    OCR0A = (period*10/1.28);
+//    counter_max = period;
 }
-
