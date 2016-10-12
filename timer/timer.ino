fa3de2d52be91f7f3e2d49f3062e17871fb3d5e8
invictus1123 <vnguyen5@stanford.edu>
2016-10-11 18:43:42 -0700

    Update timer.ino
    commit 16bit implementation

--- timer/timer.ino
+++ timer/timer.ino
@@ -11,15 +11,14 @@
 void timer_init();
 void timer_reset();
 void timer_set(uint16_t period);
-static const float CLOCK = 8E6;
-static const int PRESCALER = 64;
-static const float CNTR_FREQ = CLOCK/PRESCALER;
-static const int CNTR_MAX = 256;
-volatile uint8_t counter;
 volatile uint16_t cycle_count;
+static const float PRESCALER = 8; //prescaler for the timer.
+static const float CLOCK = 8E6; //Hz. 8MHz nominal (Pro Mini 8MHz), 16MHz for Arduino Uno.
+static const float CNTR_MAX = 65536; //the maximum value the counter can take before overflowing. 16 bit = 2^16-1; 8 bit = 2^8-1.
+static const float CNTR_FREQ = CLOCK/(PRESCALER);//the frequency in Hz of the peripheral clock/ how fast the hardware counter ticks
+//macro for running an atomic operation. Saves the global interrupt flag (whether interrupts are on or off), turns interrupts off, runs the atomic code, then rewrites interrupt flag.
 
-ISR(TIMER0_COMPA_vect){
-//    counter += 1;
+ISR(TIMER1_COMPA_vect){
       cycle_count++;
 }
 
@@ -30,27 +29,33 @@ void setup() {
 }
 
 void loop() {
-    Serial.println(cycle_count);
-//Serial.println((uint16_t) CNTR_FREQ/1000);
-//    Serial.println(TCNT0);
+  Serial.println(cycle_count);
 }
 
-void timer_init(){
-    TIMSK0 = B00000010;
-    // Toggle OC0A on match, CTC mode
-    TCCR0A = B01000010;
-    // Select clock with 1024 prescaler
-    TCCR0B = B00000011;
-    // Reset timer counter
-    TCNT0 = B00000000;
+void timer_init(){    
+    unsigned char sreg;
+    sreg=SREG;
+    cli();        
+    TCCR1A = B01000000;
+    // Use CTC mode (bits 5 and 4 from left set to 0 1) and set prescaler to 1024 (bits 3,2,and 1 from left set to 1 0 1)
+    TCCR1B = B00001010;
+  // Enable interrupts; Toggle an interrupt on match with value of OCR1A
+    TIMSK1 |= B00000010;  
+    sei();// Reset timer counter
+    
+    timer_reset();  
+    //toggle compare output mode on
 }
 
 void timer_reset(){
-    // Reset timer counter
-    TCNT0 = B00000000;
+  //do this atomically so that things don't explode if an interrupt triggers while we are accessing TCNT1.
+    unsigned char sreg;
+    sreg=SREG;
+    cli();
+    TCNT1 = 0; //16bit register
+    SREG = sreg;
 }
 
 void timer_set(uint16_t period){
-    OCR0A = CNTR_FREQ*period/1000; //left as a float so that the math with CNTR_FREQ does not overflow an integer
-//    counter_max = period;
+    OCR1A =  CNTR_FREQ*period/1000; //left as a float so that the math with CNTR_FREQ does not overflow an integer
 }
