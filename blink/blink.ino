0717fbb60273d6a2eb002df513db6a7efa3d2693
invictus1123 <vnguyen5@stanford.edu>
2016-10-11 19:54:59 -0700

    Create blink.ino

---
+++ blink/blink.ino
@@ -0,0 +1,32 @@
+/* File: blink.ino
+ * Written by Vinh Nguyen & LC Tao
+ * EE107, Sachin Katti, Fall 2016
+ * This file implements a simple library for blinking the onboard LED
+ * at a certain interval.
+ * Functionality:
+ * -Initialize LED and timer.
+ * -Set or change a period for blinking the LED. max period is 524ms, min period 8us. (Prescaler 64, 16-bit timer).
+ */
+#include "gpio.h"
+#include "led.h"
+#include "timer.h"
+
+void blink_init();
+void blink_set(uint16_t interval_ms);
+void blink_ms_timer_update();
+
+void setup() {
+}
+void loop() {
+}
+void blink_init() {
+    led_init();
+    timer_init();
+}
+void blink_set(uint16_t interval_ms) {
+    timer_set(interval_ms);
+}
+
+void blink_timer_ms_update() {
+    led_toggle();
+}
