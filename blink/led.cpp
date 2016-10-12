552ba7e5f60906198fd478e21cc20be37744909a
invictus1123 <vnguyen5@stanford.edu>
2016-10-11 19:57:07 -0700

    Create led.cpp

---
+++ blink/led.cpp
@@ -0,0 +1,32 @@
+/*  File: led.ino
+ *  Written by Vinh Nguyen & LC Tao
+ *  EE107, Sachin Katti, Fall 2016
+ *  This file implements an interface for toggling LEDs.
+ *  Functionality:
+ *  -Turn LED on or off.
+ *  -Toggle LED state.
+ */
+#include "gpio.h"
+#include "led.h"
+#include "Arduino.h"
+void led_init();
+void led_on();
+void led_off();
+void led_toggle();
+
+void led_init() {
+    gpio_set_mode(LED,1);
+    led_off();
+}
+
+void led_off() {
+    gpio_set_out(LED,0);
+}
+
+void led_on() {
+    gpio_set_out(LED,1);
+}
+
+void led_toggle() {
+    gpio_toggle_out(LED);
+}
