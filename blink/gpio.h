5035cf51774e09382acc64902c2ba31dd6cc3155
invictus1123 <vnguyen5@stanford.edu>
2016-10-25 10:33:00 -0700

    Rename blink/gpio.h to lab1/blink/gpio.h

--- blink/gpio.h
+++
@@ -1,8 +0,0 @@
-#ifndef GPIO_H
-#define GPIO_H
-#include "Arduino.h"
-
-void gpio_set_mode(uint8_t pin, uint8_t out_or_in);
-void gpio_set_out(uint8_t pin, uint8_t on_or_off);
-void gpio_toggle_out(uint8_t pin);
-#endif // GPIO_H
