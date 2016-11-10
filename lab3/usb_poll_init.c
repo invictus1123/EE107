dec9ebeb46b742fa8ca5dadb37f3940fb5eb7819
Lctao <LCTAO13@gmail.com>
2016-11-10 01:57:01 -0800

    Bug fix for perror line, added fcntl statement

--- lab3/usb_poll_init.c
+++ lab3/usb_poll_init.c
@@ -2,11 +2,13 @@
 
 int usb_poll_init(){
     int fd; // File descriptor for serial port
+
     fd = open(“dev/ttyUSB0”, O_RDWR | NOCTTY);
     if (fd == -1){
-        perror(“”Could not open serial port USB0);
+        perror(“Could not open serial port USB0");
         return -1;
     }
     
-	 return fd;
+    fcntl(fd, F_SETFL, 0); // Not sure if this is necessary
+    return fd;
 }
