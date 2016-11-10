686e081f786e579e5b7d664539599df9b5578329
Lctao <lctao13@gmail.com>
2016-11-10 14:59:44 -0800

    Working version of Wifi TX

--- lab3/imu_data_txrx.c
+++ lab3/imu_data_txrx.c
@@ -1,29 +1,34 @@
-#include “imu_data_txrx.h”
+#include "imu_data_txrx.h"
 
 int imu_data_txrx(int usb_id, int *store_val, int *cnt, uint8_t *buf){
     int rx_count;
     int timestamp;
 
-    rx_count = read(usb_id, store_val, 1); // Read 1 byte (block until byte is read, or timeout occurs)
+    rx_count = read(usb_id, (void *)store_val, 1); // Read 1 byte (block until byte is read, or timeout occurs)
     timestamp = (int) time(NULL);    // Take timestamp immediately after reading (even if unused)
 
-    if (rx_count == -1){
+    if (rx_count != 1){
         // Read error
+	printf("Read error\n");
+	*cnt = 0;
+	return 0;
     }
-	 
+    printf("Count: %d\n", *cnt); 
+    printf("Store_val: %d\n", *store_val);
+    printf("Timestamp: %d\n", timestamp);
     /* Store timestamp in transmit buffer if last byte */
-    if (*cnt == 3){
-        buf[3] = (uint8_t) *store_val;
-        buf[4] = (timestamp >> 24) & 0xFF;
-        buf[5] = (timestamp >> 16) & 0xFF;
-        buf[6] = (timestamp >> 8) & 0xFF;
-        buf[7] = timestamp & 0xFF;
+    if (*cnt == 13){
+        buf[13] = (uint8_t) *store_val;
+        buf[14] = (timestamp >> 24) & 0xFF;
+        buf[15] = (timestamp >> 16) & 0xFF;
+        buf[16] = (timestamp >> 8) & 0xFF;
+        buf[17] = timestamp & 0xFF;
 
         *cnt = 0;
         return 1;
     } else {
-        *cnt++;
-        buf[cnt] = (uint8_t) *store_val;
+        buf[*cnt] = (uint8_t) *store_val;
+        *cnt = *cnt + 1;
         return 0;
     }
 
--- lab3/usb_poll_init.c
+++ lab3/usb_poll_init.c
@@ -1,14 +1,20 @@
 #include "usb_poll_init.h"
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <fcntl.h>
+
 int usb_poll_init(){
     int fd; // File descriptor for serial port
 
-    fd = open(“dev/ttyUSB0”, O_RDWR | NOCTTY);
+    fd = open("/dev/ttyUSB0", O_RDONLY);
     if (fd == -1){
-        perror(“Could not open serial port USB0");
+	printf("Error\n");
+        //perror("Could not open serial port USB0");
         return -1;
     }
     
-    fcntl(fd, F_SETFL, 0); // Not sure if this is necessary
+    // fcntl(fd, F_SETFL, 0); // Not sure if this is necessary
     return fd;
 }
