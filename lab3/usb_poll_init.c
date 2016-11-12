4f8723ccca2b8cebe3433ea4d46e0b73a6b1da57
Lctao <LCTAO13@gmail.com>
2016-11-11 17:20:32 -0800

    Add perror statement

--- lab3/usb_poll_init.c
+++ lab3/usb_poll_init.c
@@ -3,15 +3,14 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stdio.h>
-#include <fcntl.h>
+#include <errno.h>
 
 int usb_poll_init(){
     int fd; // File descriptor for serial port
 
     fd = open("/dev/ttyUSB0", O_RDONLY);
     if (fd == -1){
-	printf("Error\n");
-        //perror("Could not open serial port USB0");
+        perror("Could not open serial port USB0");
         return -1;
     }
     
