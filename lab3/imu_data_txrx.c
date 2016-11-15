db00b2d158aa1bf212a938904044c1933e5ede3f
Lctao <LCTAO13@gmail.com>
2016-11-14 20:22:41 -0800

    Updated to match new START_BYTE and STOP_BYTE spec

--- lab3/imu_data_txrx.c
+++ lab3/imu_data_txrx.c
@@ -1,36 +1,40 @@
 #include "imu_data_txrx.h"
 
+#define START_BYTE -128
+#define STOP_BYTE 127
+
 int imu_data_txrx(int usb_id, int *store_val, int *cnt, uint8_t *buf){
     int rx_count;
     int timestamp;
+	int8_t read_buf;
 
-    rx_count = read(usb_id, (void *)store_val, 1); // Read 1 byte (block until byte is read, or timeout occurs)
-    timestamp = (int) time(NULL);    // Take timestamp immediately after reading (even if unused)
-
+    rx_count = read(usb_id, &read_buf, 1); // Read 1 byte (block until byte is read, or timeout occurs)
+	
+	// Read error
     if (rx_count != 1){
-        // Read error
-	printf("Read error\n");
-	*cnt = 0;
+		printf("Read error\n");
+		*cnt = 0;
+		return 0;
+    }
+	// Stop byte read
+	if (read_buf == STOP_BYTE){
+		// Append timestamp to buf
+		timestamp = (int) time(NULL);
+		buf[*cnt + 1] = (timestamp >> 24) & 0xFF;
+        buf[*cnt + 2] = (timestamp >> 16) & 0xFF;
+        buf[*cnt + 3] = (timestamp >> 8) & 0xFF;
+        buf[*cnt + 4] = timestamp & 0xFF;
+		// Reset store_val and cnt
+		*store_val = 0;
+		*cnt = 0;
+		return 1; //Complete IMU data (including timestamp) has been read
+	// Start byte read
+	} else if (read_buf == START_BYTE){
+		*store_val = 1;
+	} else if (*store_val == 1){
+		buf[*cnt] = (uint8_t) *read_buf;
+		*cnt = *cnt + 1;
+	}
+	
 	return 0;
-    }
-    printf("Count: %d\n", *cnt); 
-    printf("Store_val: %d\n", *store_val);
-    printf("Timestamp: %d\n", timestamp);
-    /* Store timestamp in transmit buffer if last byte */
-    if (*cnt == 13){
-        buf[13] = (uint8_t) *store_val;
-        buf[14] = (timestamp >> 24) & 0xFF;
-        buf[15] = (timestamp >> 16) & 0xFF;
-        buf[16] = (timestamp >> 8) & 0xFF;
-        buf[17] = timestamp & 0xFF;
-
-        *cnt = 0;
-        return 1;
-    } else {
-        buf[*cnt] = (uint8_t) *store_val;
-        *cnt = *cnt + 1;
-        return 0;
-    }
-
-    
 }
