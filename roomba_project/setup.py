aeab7373843327b887092d36ac69f69535a82c94
invictus1123 <vnguyen5@stanford.edu>
2016-12-09 12:31:03 -0800

    Update setup.py

--- roomba_project/setup.py
+++ roomba_project/setup.py
@@ -1,6 +1,7 @@
 #Initializes a Roomba_Position object and the desired waypoints
 import roomba_class
 def setup()
-waypoints = [(50,50)];
-emulator = Roomba_Position(waypoints[0][0], waypoints[0][1]); #initialize with the first waypoints as the target
+  waypoints = [(50,50)];
+  current_waypoint = 0
+  emulator = Roomba_Position(waypoints[0][0], waypoints[0][1]); #initialize with the first waypoints as the target
 
