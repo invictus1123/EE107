0dd367c01605ac441a9b1af212ef985ff33912b2
invictus1123 <vnguyen5@stanford.edu>
2016-12-15 10:44:30 -0800

    Update move_commands.py
    added roomba commands
    constants will need velocity constants
    class will need additional parameters to these methods, namely a create2 object

--- roomba_project/move_commands.py
+++ roomba_project/move_commands.py
@@ -1,17 +1,26 @@
-def roomba_forward():
+def roomba_forward(bot):
 	print "Roomba moving forward"
+	bot.drive_straight(DRIVE_V);
 	return 0;
 
-def roomba_turn(angle):
+def roomba_turn(angle,bot):
 	print "Roomba turning %f radians clockwise" % angle;
+	bot.turn_clockwise(TURN_V);
 	return 0;
 
-def roomba_stop():
+def roomba_stop(bot):
 	print "Roomba stopping"
+	bot.drive(0,0);
 	return 0;
 
-def roomba_sound():
+def roomba_sound(bot):
+	bot.play_test_sound();
 	return 0;
 
-def roomba_light():
+def roomba_light(bot,RG_val):
+	#turns the power button LED a particular color based on the RG value
+	#0 = green, 255 = red, 128 = yellow
+	#data to send to the bot: opcode = 139, code for lighting power LED = 4, RG value, Intensity = 255)
+	data = (139,4,RG_val,255);
+	bot.SCI.send(bot.config.data['opcodes']['led'],data);
 	return 0;
