3005adf45f304fe8c707cc13c872ebda246c6121
invictus1123 <vnguyen5@stanford.edu>
2016-12-15 11:51:22 -0800

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,9 @@
+SIZEOF_INT = 4;
+SIZEOF_FLOAT = 4;
+lengthOfData = 12;
+MAX_ACTION_COUNT = 20;	# Number of loops between Roomba action
+TURN_V = 100;
+DRIVE_V = 100;
+
+THRESHOLD = 5;			# Threshold for distance equality (mm)
+DONE_FLAG = -1;			# Flag to indicate last waypoint reached
